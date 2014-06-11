9f252f17205ad19b264937a1e3492975bf9bba95
rubyu <ruby.u.g@gmail.com>
2014-06-11 16:32:56 +0900

    入力が空の場合のstderrorへの出力を抑制。

--- eb-audio.bat
+++ eb-audio.bat
@@ -27,7 +27,12 @@ if not defined d (
 
 java -jar ad-edit-0.0.0.jar %c% %t% --format wav --exec ^
     java -Dfile.encoding=utf-8 -jar ebquery-0.3.1.jar -f html -d %d% -m sd "${ field(%source%) }" ^| ^
+    gnupack_basic-11.00\app\cygwin\cygwin\bin\gawk "{ print $0 } END { print \"\n\" }" ^| ^
     gnupack_basic-11.00\app\cygwin\cygwin\bin\sed -e "s@\\(</audio>\\)@\\1\\n@g" ^| ^
+    gnupack_basic-11.00\app\cygwin\cygwin\bin\sed -e "$s@$@\n\n\n\n\n\n\n\n\n@" ^| ^
+    gnupack_basic-11.00\app\cygwin\cygwin\bin\sed -e "s@^@<?xml version=\"\"1.0\"\" ?><entry>@g" ^| ^
+    gnupack_basic-11.00\app\cygwin\cygwin\bin\sed -e "s@$@</entry>@g" ^| ^
     gnupack_basic-11.00\app\cygwin\cygwin\bin\sed -n "1p" ^| ^
     gnupack_basic-11.00\app\cygwin\cygwin\bin\xmllint --nowarning --xpath "substring-after(//audio[position()=1]/@src, \"\"base64,\"\")" - ^| ^
     gnupack_basic-11.00\app\cygwin\cygwin\bin\base64 -d
+    
