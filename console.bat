4b7ea160264d17de3b252400eb2f0cd85fdc440e
rubyu <ruby.u.g@gmail.com>
2014-06-12 16:11:30 +0900

    consoleを起動するバッチファイルを追加。

---
+++ console.bat
@@ -0,0 +1,10 @@
+@title ad-tools console
+@echo ==== ad-tools console ====
+@echo;
+@echo Setting environment parameters ...
+set PATH=recipe;ad;dep\gnupack_basic-11.00\app\cygwin\cygwin\bin;%PATH%
+@echo;
+@echo done.
+@echo;
+@echo off
+cmd.exe /k
